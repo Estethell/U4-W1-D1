b73a79b570b59b61c1c8bc7e4b027dafde339cbb
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-04-09 17:31:32 +0200

    commit

--- index.php
+++ index.php
@@ -146,7 +146,63 @@ $partite = [
 ],
 
     
-]
+];
+
+$italian_days = [
+"Domenica",
+"Lunedì",
+"Martedì",
+"Mercoledì",
+"Giovedì",
+"Venerdì",
+"Sabato",
+
+];
+
+$num_month = date('n');
+switch ($num_month) {
+    case '1':
+        $month_ita = "gennaio";
+        break;
+    case '2':
+        $month_ita = "febbraio";
+        break;
+    case '3':
+        $month_ita = "marzo";
+        break;
+    case '4':
+        $month_ita = "aprile";
+        break;
+    case '5':
+        $month_ita = "maggio";
+        break;
+    case '6':
+        $month_ita = "giugno";
+        break;
+    case '7':
+        $month_ita = "luglio";
+        break;
+    case '8':
+        $month_ita = "agosto";
+        break;
+    case '9':
+        $month_ita = "settembre";
+        break;
+    case '10':
+        $month_ita = "ottobbre";
+        break;
+    case '11':
+        $month_ita = "novembre";
+        break;
+    case '12':
+        $month_ita = "dicembre";
+        break;
+}
+
+$day_ita = $italian_days[date('w')];
+ $day_num = date('j');
+  
+   $year = date('Y');
 
 
 ?>
@@ -161,8 +217,8 @@ $partite = [
 <body>
    <h1><?php
 
-date_default_timezone_set('CET');
-echo date('l jS \of F Y ');
+
+echo "$day_ita, $day_num $month_ita $year" 
 
 ?></h1> 
 
